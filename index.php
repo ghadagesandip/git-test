43453560060e92a064ff2eb94fe18f6ccaa5380c
sandip home <ghadagesandip@gmail.com>
2014-09-19 18:11:04 +0530

    updated index

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   echo "<br/>changes from hot fix branch";
   echo "<br/>in issue branch 1";
   echo "<br/>in issue branch 2";
-  echo "<br/> finished branch 2"
-  echo "Good luck"
+  echo "<br/> finished branch 2";
+  echo "Good luck";
 
