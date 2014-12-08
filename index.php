f1cb3386b5621b75eab4250433ac381c96b6d7a0
sandip ghadage <ghadagesandip@gmail.com>
2014-12-08 17:06:30 +0530

    updated index file, removed previous content

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
 <?php
-  echo "hello";
-  echo "<br/>changes from hot fix branch";
-  echo "<br/>in issue branch 1";
-  echo "<br/>in issue branch 2";
-  echo "<br/> finished branch 2";
-  echo "<br/>Good luck";
-  echo "<br/>Dev ";
-
+ echo "hello";
